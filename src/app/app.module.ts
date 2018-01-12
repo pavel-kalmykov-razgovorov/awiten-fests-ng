319040ae0a94f7162de0562d10082d2b8cbcd072
Ramsés Martínez <rmm118@alu.ua.es>
2018-01-12 17:16:13 +0100

    Header, Festivals and Artists basic componenets added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FestivalsListComponent } from './festivals/festivals-list/festivals-list.component';
+import { ArtistsListComponent } from './artists/artists-list/artists-list.component';
+import { FestivalsComponent } from './festivals/festivals.component';
+import { ArtistsComponent } from './artists/artists.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FestivalsListComponent,
+    ArtistsListComponent,
+    FestivalsComponent,
+    ArtistsComponent
   ],
   imports: [
     BrowserModule
