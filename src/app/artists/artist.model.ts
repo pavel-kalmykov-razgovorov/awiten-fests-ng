08d7b40454a3fd4ece8432cffdeae2b216e0a873
Ramsés Martínez <rmm118@alu.ua.es>
2018-01-17 01:45:02 +0100

    Basic constructors added

--- src/app/artists/artist.model.ts
+++ src/app/artists/artist.model.ts
@@ -1,4 +1,9 @@
 export class Artist {
   public name: string;
   public website: string;
+
+  constructor(name: string, website: string) {
+    this.name = name;
+    this.website = website;
+  }
 }
