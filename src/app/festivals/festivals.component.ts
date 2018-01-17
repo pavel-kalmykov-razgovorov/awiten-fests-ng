9f4abc55d34f1a9b1e2b21f35badffdfc2300c4d
Ramsés Martínez <rmm118@alu.ua.es>
2018-01-17 02:18:00 +0100

    Added basic componenet services

--- src/app/festivals/festivals.component.ts
+++ src/app/festivals/festivals.component.ts
@@ -1,18 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Festival } from "./festival.model";
+import { FestivalService } from "./festival.service";
 
 @Component({
   selector: 'app-festivals',
   templateUrl: './festivals.component.html',
-  styleUrls: ['./festivals.component.css']
+  styleUrls: ['./festivals.component.css'],
+  providers: [FestivalService]
 })
 export class FestivalsComponent implements OnInit {
-  festivals: Festival[] = [
-    new Festival('A Test Festival','This is a random location')
-  ];
-  constructor() { }
+  festivals: Festival[];
+  constructor(private festivalService: FestivalService) { }
 
   ngOnInit() {
+    this.festivals = this.festivalService.getFestivals();
   }
 
 }
